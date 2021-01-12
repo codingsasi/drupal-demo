5c047dcbe37a0a2d98f523ad57f1cf2d20164615
Abhai Sasidharan <abhaisasidharan@gmail.com>
2021-01-13 01:23:25 +0530

    flexslider and superfish

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -791,17 +791,17 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
  *
  * Keep this code block at the end of this file to take full effect.
  */
-#
-# if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
-#   include $app_root . '/' . $site_path . '/settings.local.php';
-# }
 $databases['default']['default'] = array (
   'database' => 'drupal8',
   'username' => 'drupal8',
   'password' => 'drupal8',
   'prefix' => '',
-  'host' => 'database',
+  'host' => 'localhost',
   'port' => '3306',
   'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
   'driver' => 'mysql',
 );
+
+if (file_exists($app_root . '/' . $site_path . '/settings.local.php') && getenv('LANDO') === 'ON') {
+  include $app_root . '/' . $site_path . '/settings.local.php';
+}
